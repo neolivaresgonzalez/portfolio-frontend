c9bca8bb34b65b2014fd5b58babf0741272f8498
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-10 14:55:28 -0500

    Created main skeleton components + added TailwindCSS + shadcn/ui + Animated UI Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HeaderNavbar } from '@/components/header-navbar/header-navbar'
+import { HeroSection } from '@/components/hero/hero'
+import { AboutSection } from '@/components/about/about'
+import { SkillsSection } from '@/components/skills/skills'
+import { ContactSection } from '@/components/contact/contact'
+import { ProjectsSection } from '@/components/projects/projects'
+import { FooterNavbar } from '@/components/footer-navbar/footer-navbar'
+
+function Portfolio() {
+  return (
+    <div className="flex flex-col gap-10 width-screen align-center justify-center">
+      <HeaderNavbar />
+      <HeroSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+      <FooterNavbar />
+    </div>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Portfolio />
   )
 }
 
---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,7 @@
+export function ProjectsSection() {
+    return (
+        <div className="flex flex-1">
+            <h1>ProjectsSection</h1>
+        </div>
+    )
+}
