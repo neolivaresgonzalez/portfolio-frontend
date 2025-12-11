c2535a890ab6eaaf5ce6b5a1e8b7f93b9d7203b0
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-11 11:26:30 -0500

    Basic elements aligning

--- src/components/ui/footer-navbar.tsx
+++ src/components/ui/footer-navbar.tsx
@@ -1,7 +1,8 @@
 export function FooterNavbar() {
     return (
-        <div className="flex flex-1">
-            <h1>FooterNavbar</h1>
+        <div className="flex flex-col h-full">
+            <div className="flex flex-1 items-center justify-center">Navbar</div>
+            <div className="flex flex-1 items-center justify-center">Footer Credits</div>
         </div>
     )
 }
