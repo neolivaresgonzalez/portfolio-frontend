02ddadcf8e1d7fc89a4cf33564b0b1b51b14a568
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-12 00:10:33 -0500

    Changes defaultOpen status to false on SidebarProvider

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import { AppSidebar } from "@/components/ui/app-sidebar"
 
 export function Layout({ children }: { children: React.ReactNode }) {
     return (
-        <SidebarProvider>
+        <SidebarProvider defaultOpen={false}>
             <AppSidebar />
             <SidebarInset>
                 <header className="sticky max-w-full top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b bg-background px-4">
