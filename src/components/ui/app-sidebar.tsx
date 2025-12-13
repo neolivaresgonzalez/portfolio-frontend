70bb1b1b588ccd5fdb6f6013af6b41e3d6b9fbc7
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-12 19:57:59 -0500

    refactor: Remove unused icon imports, comment out portfolio sidebar items, add MenuItem type, and conditionally render the portfolio section.

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -3,10 +3,10 @@ import { UserIcon } from "@/components/ui/icons/user"
 import { FolderCodeIcon } from "@/components/ui/icons/folder-code"
 import { SparklesIcon } from "@/components/ui/icons/sparkles"
 import { AtSignIcon } from "@/components/ui/icons/at-sign"
-import { SearchIcon } from "@/components/ui/icons/search"
-import { CoffeeIcon } from "@/components/ui/icons/coffee"
-import { RocketIcon } from "@/components/ui/icons/rocket"
-import { HeartIcon } from "@/components/ui/icons/heart"
+// import { SearchIcon } from "@/components/ui/icons/search"
+// import { CoffeeIcon } from "@/components/ui/icons/coffee"
+// import { RocketIcon } from "@/components/ui/icons/rocket"
+// import { HeartIcon } from "@/components/ui/icons/heart"
 
 import {
     Sidebar,
@@ -19,8 +19,15 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+type MenuItem = {
+    title: string;
+    url: string;
+    icon: React.ComponentType<{ className?: string }>;
+}
+
+
 // Menu items mapping to home sections
-const homeItems = [
+const homeItems: MenuItem[] = [
     {
         title: "Home",
         url: "#hero",
@@ -48,34 +55,36 @@ const homeItems = [
     },
 ]
 
+
+
 // Menu items mapping to portfolio routes (feautured projects, professionals, personal, hobbies)
-// TODO: Add portfolio routes to project search component with filtered results
-const portfolioItems = [
-    {
-        title: "Search Projects",
-        url: "#search-projects",
-        icon: SearchIcon,
-    },
-    {
-        title: "Featured Projects",
-        url: "#featured-projects",
-        icon: SparklesIcon,
-    },
-    {
-        title: "Professionals",
-        url: "#professionals",
-        icon: CoffeeIcon,
-    },
-    {
-        title: "Personal & Hobbies",
-        url: "#personal",
-        icon: RocketIcon,
-    },
-    {
-        title: "Volunteer",
-        url: "#volunteer",
-        icon: HeartIcon,
-    }
+// TODO:When project search component is implemented, add it to the menu items routing to filtered results
+const portfolioItems: MenuItem[] = [
+    // {
+    //     title: "Search Projects",
+    //     url: "#search-projects",
+    //     icon: SearchIcon,
+    // },
+    // {
+    //     title: "Featured Projects",
+    //     url: "#featured-projects",
+    //     icon: SparklesIcon,
+    // },
+    // {
+    //     title: "Professionals",
+    //     url: "#professionals",
+    //     icon: CoffeeIcon,
+    // },
+    // {
+    //     title: "Personal & Hobbies",
+    //     url: "#personal",
+    //     icon: RocketIcon,
+    // },
+    // {
+    //     title: "Volunteer",
+    //     url: "#volunteer",
+    //     icon: HeartIcon,
+    // }
 ]
 
 export function AppSidebar() {
@@ -100,23 +109,27 @@ export function AppSidebar() {
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Portfolio</SidebarGroupLabel>
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            {portfolioItems.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
+                {
+                    portfolioItems.length > 0 && (
+                        <SidebarGroup>
+                            <SidebarGroupLabel>Portfolio</SidebarGroupLabel>
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    {portfolioItems.map((item) => (
+                                        <SidebarMenuItem key={item.title}>
+                                            <SidebarMenuButton asChild>
+                                                <a href={item.url}>
+                                                    <item.icon />
+                                                    <span>{item.title}</span>
+                                                </a>
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    ))}
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                        </SidebarGroup>
+                    )
+                }
             </SidebarContent>
         </Sidebar>
     )
