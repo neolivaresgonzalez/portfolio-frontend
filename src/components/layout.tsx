da4f6c7aae7cfca66a8b33b7280930fb65a8b65b
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2025-12-16 15:44:51 -0500

    SidebarInset now stretches when opening the sidebar. Contents gets shrinked on the <main> inside element

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider defaultOpen={false}>
             <AppSidebar />
-            <SidebarInset>
+            <SidebarInset className="min-w-0">
                 <header className="sticky max-w-full top-0 z-10 flex h-12 max-h-12 shrink-0 items-center gap-2 border-b bg-background px-4">
                     <SidebarTrigger className="-ml-1" />
                     <div className="flex items-center gap-2">
