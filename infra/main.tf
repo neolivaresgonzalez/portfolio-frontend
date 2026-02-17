38ba899bfba4f7c3535fe172d6ef25317cee5a72
neolivaresgonzalez <ne.olivaresgonzalez@gmail.com>
2026-02-16 23:17:31 -0500

    updated main terraform script

--- infra/main.tf
+++ infra/main.tf
@@ -21,8 +21,14 @@ provider "aws" {
 }
 
 variable "domain_name" {
+  default = "nolivares.com"
+}
+
+variable "legacy_domain_name" {
   default = "neolivaresgonzalez.com"
 }
+
+# --- Main Domain Resources (nolivares.com) ---
 
 resource "random_id" "frontend_bucket_suffix" {
   byte_length = 4
@@ -75,44 +81,28 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
   depends_on = [aws_s3_bucket_public_access_block.frontend_public_access]
 }
 
-# --- Route53 & ACM ---
-
-data "aws_route53_zone" "main" {
-  name = var.domain_name
-}
-
+# Main Domain Cert (DNS Validation via Cloudflare - Manual CNAMEs required)
 resource "aws_acm_certificate" "cert" {
-  provider          = aws.us_east_1
-  domain_name       = var.domain_name
+  provider                  = aws.us_east_1
+  domain_name               = var.domain_name
   subject_alternative_names = ["www.${var.domain_name}"]
-  validation_method = "DNS"
+  validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => dvo
-  }
-
-  allow_overwrite = true
-  name            = each.value.resource_record_name
-  records         = [each.value.resource_record_value]
-  ttl             = 60
-  type            = each.value.resource_record_type
-  zone_id         = data.aws_route53_zone.main.zone_id
-}
-
+# Wait for certificate validation
 resource "aws_acm_certificate_validation" "cert" {
-  provider                = aws.us_east_1
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  provider        = aws.us_east_1
+  certificate_arn = aws_acm_certificate.cert.arn
 }
 
-# --- CloudFront ---
+# Note: No automatic Route53 validation for main domain as it is on Cloudflare.
+# The user must add the CNAME records outputted below to Cloudflare.
 
+# Main Domain CloudFront
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name = aws_s3_bucket_website_configuration.frontend_website.website_endpoint
@@ -163,29 +153,134 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 }
 
-resource "aws_route53_record" "apex" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.domain_name
+# --- Legacy Domain Resources (neolivaresgonzalez.com -> nolivares.com) ---
+
+# Legacy Zone Data
+data "aws_route53_zone" "legacy" {
+  name = var.legacy_domain_name
+}
+
+# Redirect Bucket
+resource "aws_s3_bucket" "redirect_bucket" {
+  bucket = "portfolio-redirect-${random_id.frontend_bucket_suffix.hex}"
+}
+
+resource "aws_s3_bucket_website_configuration" "redirect_website" {
+  bucket = aws_s3_bucket.redirect_bucket.id
+
+  redirect_all_requests_to {
+    host_name = var.domain_name
+    protocol  = "https"
+  }
+}
+
+# Legacy Cert (Validated via Route53)
+resource "aws_acm_certificate" "legacy_cert" {
+  provider                  = aws.us_east_1
+  domain_name               = var.legacy_domain_name
+  subject_alternative_names = ["www.${var.legacy_domain_name}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "legacy_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.legacy_cert.domain_validation_options : dvo.domain_name => dvo
+  }
+
+  allow_overwrite = true
+  name            = each.value.resource_record_name
+  records         = [each.value.resource_record_value]
+  ttl             = 60
+  type            = each.value.resource_record_type
+  zone_id         = data.aws_route53_zone.legacy.zone_id
+}
+
+resource "aws_acm_certificate_validation" "legacy_cert_validation" {
+  provider                = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.legacy_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.legacy_cert_validation : record.fqdn]
+}
+
+# Legacy CloudFront (for Redirect)
+resource "aws_cloudfront_distribution" "legacy_distribution" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.redirect_website.website_endpoint
+    origin_id   = "S3-Redirect-${aws_s3_bucket.redirect_bucket.bucket}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  aliases = [var.legacy_domain_name, "www.${var.legacy_domain_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Redirect-${aws_s3_bucket.redirect_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all" # S3 website redirect handles protocol, but cloudfront can too.
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.legacy_cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+# Legacy DNS Records
+resource "aws_route53_record" "legacy_apex" {
+  zone_id = data.aws_route53_zone.legacy.zone_id
+  name    = var.legacy_domain_name
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.legacy_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.legacy_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
-resource "aws_route53_record" "www" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = "www.${var.domain_name}"
+resource "aws_route53_record" "legacy_www" {
+  zone_id = data.aws_route53_zone.legacy.zone_id
+  name    = "www.${var.legacy_domain_name}"
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.legacy_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.legacy_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
+
+# --- Outputs ---
 
 output "frontend_bucket_name" {
   value = aws_s3_bucket.frontend_bucket.bucket
@@ -194,4 +289,16 @@ output "frontend_bucket_name" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.s3_distribution.domain_name
 }
+
+output "manual_dns_validation_records" {
+  description = "CNAME records to add to Cloudflare for nolivares.com SSL validation"
+  value = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      value = dvo.resource_record_value
+      type  = dvo.resource_record_type
+    }
+  }
+}
+
 
